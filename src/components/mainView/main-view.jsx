fa65c97a93b4b6556b6bbdb6c6a6723b49547a7b
Mezekr W. <66778493+Mezekr@users.noreply.github.com>
2024-02-27 08:18:02 +0000

    add useState to books

--- src/components/mainView/main-view.jsx
+++ src/components/mainView/main-view.jsx
@@ -1,11 +1,22 @@
+import { useState } from "react";
+import { BookCard } from "../bookCard/BookCard";
+
 export const MainView = () => {
+  const [books, setBooks] = useState([
+    { id: 1, title: "Eloquent JavaScript" },
+    { id: 2, title: "Mastering JavaScript Functional Programming" },
+    { id: 3, title: "JavaScript: The Good Parts" },
+    { id: 4, title: "JavaScript: The Definitive Guide" },
+    { id: 5, title: "The Road to React" },
+  ]);
+  if (books === 0) return <div>Books list is empty!</div>;
+
   return (
     <div>
-      <div>Eloquent JavaScript</div>
-      <div>Mastering JavaScript Functional Programming</div>
-      <div>JavaScript: The Good Parts</div>
-      <div>JavaScript: The Definitive Guide</div>
-      <div>The Road to React</div>
+      {books.map((book) => {
+        // return <div key={book.id}>{book.title}</div>;
+        return <BookCard book={book} />;
+      })}
     </div>
   );
 };
