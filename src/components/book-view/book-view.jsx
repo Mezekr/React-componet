bd793819485b1d1587f481212087a444c9c3b933
Mezekr W. <66778493+Mezekr@users.noreply.github.com>
2024-03-02 09:07:33 +0000

    refactor book-view component

--- src/components/book-view/book-view.jsx
+++ src/components/book-view/book-view.jsx
@@ -1,16 +1,18 @@
 export const BookView = ({ book, onBackClick }) => {
   return (
-    <>
+    <div className="book">
       <img src={book.image} />
-      <div>
-        <span>Title: </span>
-        <span>{book.title}</span>
-      </div>
-      <div>
-        <span>Author: </span>
-        <span>{book.author}</span>
+      <div className="book-detail">
+        <div>
+          <span>Title: </span>
+          <span>{book.title}</span>
+        </div>
+        <div>
+          <span>Author: </span>
+          <span>{book.author}</span>
+        </div>
       </div>
       <button onClick={onBackClick}>Back</button>
-    </>
+    </div>
   );
 };
